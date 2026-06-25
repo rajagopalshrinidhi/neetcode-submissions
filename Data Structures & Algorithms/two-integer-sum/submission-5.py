f73ba3e712c14c476f82c038bcc86a801d987682
rajagopalshrinidhi <rajagopalshrinidhi@gmail.com>
2026-06-25 15:42:24 -0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # result = []
+        n = len(nums)
+
+        # I may need a hashmap with indices and values
+        # so that I can easily find a number nums[j], such that
+        # target-nums[i]=nums[j].
+
+        # The brute force way will be to have two nested loops,
+        # which will increase our time complexity to O(n^2)
+
+        hashMap = {}
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in hashMap:
+                return [hashMap[complement][0],i]
+            if nums[i] not in hashMap:
+                hashMap[nums[i]] = []
+            hashMap[nums[i]].append(i)
+
